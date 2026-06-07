9b8abea6a13413399d4158552e2b6622047fe339
Shoban Chiddarth <messageshoban@gmail.com>
2026-06-07 17:43:53 +0530

    Created basic gin app on main.go

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+func init() {
+
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+		"message": "pong",
+		})
+	})
+	router.Run(":"+os.Getenv("PORT")) // listens on 0.0.0.0:8080 by default
+}
