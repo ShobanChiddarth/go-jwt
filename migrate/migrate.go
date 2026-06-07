64c34df67e5b6c35646fa978b0b68dfb74704a34
Shoban Chiddarth <messageshoban@gmail.com>
2026-06-07 17:54:46 +0530

    Added DB migration script (standalone)

---
+++ migrate/migrate.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/shobanchiddarth/go-jwt/initializers"
+	"github.com/shobanchiddarth/go-jwt/models"
+)
+
+func init() {
+	initializers.ConnectToDB()
+}
+
+func main() {
+	initializers.DB.AutoMigrate(&models.User{})
+}
